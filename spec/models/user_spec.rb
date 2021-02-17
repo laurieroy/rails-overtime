12538a4b63a69ff6cadd57d61c6f92c203e9c840
lauretteroy <laurieroy.dev@gmail.com>
2021-02-16 22:47:56 -0700

    Refactor test, add post index, table, post partial. custom format name

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,20 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  before do
+    @user = User.create(email: "test@test.com", 
+      password: "password", password_confirmation: "password", first_name: "test", last_name: "tester" )
+  end
   describe "creation" do
-    before do
-      @user = User.create(email: "test@test.com", 
-        password: "password", password_confirmation: "password", first_name: "test", last_name: "tester" )
-    end
-
     it "can be created" do
       expect(@user).to be_valid
-    end
+  end
 
     it "cannot be created without first_name last_name" do
       @user.first_name = nil
       @user.last_name = nil
       expect(@user).to_not be_valid
+    end
+  end
+
+  describe "custom name methods" do
+    it 'has a full name method that combines first and last name' do
+      expect(@user.name.sorted.upcase).to eq("TESTER, TEST")
     end
   end
 end

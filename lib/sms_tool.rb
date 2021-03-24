89ff3b8bfdb66f4b2544848d4d3ad4cdfd0c97af
lauretteroy <laurieroy.dev@gmail.com>
2021-03-24 15:33:55 -0600

    Update twilio_phone

--- lib/sms_tool.rb
+++ lib/sms_tool.rb
@@ -2,15 +2,13 @@ module SmsTool
 	require 'twilio-ruby'
 	account_sid = Rails.application.credentials.twilio[:account_sid]
 	auth_token = Rails.application.credentials.twilio[:auth_token]
+	twilio_phone = Rails.application.credentials.twilio[:phone_number]
 
 	@client = Twilio::REST::Client.new(account_sid, auth_token)
 
 	def self.send_sms(number:, message:)
 		@client.messages.create(
-			from: '+18588793452',
-			# from: Rails.application.credentials.twilio[:phone_number],
-			# I stored my # w/+1, so have to add back in for to #
-		
+			from: twilio_phone,
 			# to: "+19713195769",
 			to: "+1#{number}",
 			body: "#{message}"

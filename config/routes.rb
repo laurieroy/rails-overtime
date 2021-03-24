74ddebfae06a0e649a162b52e31deea89e0d6ab4
lauretteroy <laurieroy.dev@gmail.com>
2021-03-24 14:16:23 -0600

    Move status_badge, add enums, build and update audit_log index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
-  resources :audit_logs
+  resources :audit_logs, except: [:new, :edit, :destroy]
+
   namespace :admin do
       resources :posts
       resources :users
